85d70b28d0bc74b188bb7f94b6988cb3752be8eb
det-api <digitalengineeringtech.api@gmail.com>
2023-05-02 20:39:27 +0630

    2

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ port.on('data', (data) => {
 port.on('error', (err) => {
   console.error('Error:', err.message);
 });
+
+let gg = true
+while (gg = true) {
+  port.on('open', () => {
+    const data = Buffer.from([0x01, 0x30, 0x31, 0x02, 0x54, 0x51, 0x03, 0x05]);
+    port.write(data, (err) => {
+      if (err) {
+        console.error('Error sending data: ', err);
+      } else {
+        console.log('Data sent successfully');
+      }
+    });
+  });
+}
